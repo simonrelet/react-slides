39b547ec9a593d3292743e465b6fde57bee52f7b
Simon Relet <simon.relet@gmail.com>
2016-05-08 21:02:24 +0200

    Loading code

--- src/slides/Welcome.jsx
+++ src/slides/Welcome.jsx
@@ -3,13 +3,14 @@
 import React, { PropTypes } from 'react';
 import FooterSlide from '../templates/containers/FooterSlide';
 import ColumnSlide from '../templates/containers/ColumnSlide';
+import titleCode from '!!raw!./Title.jsx';
 
 export default function Welcome(props) {
   return (
     <FooterSlide config={ props.config }>
       <ColumnSlide title='Hello World'>
       <div>Toto</div>
-      <div>tata</div>
+      <pre>{ titleCode }</pre>
       </ColumnSlide>
     </FooterSlide>
   );
